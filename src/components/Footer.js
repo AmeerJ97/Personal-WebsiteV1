12bc5da267a132c03442c390e7a2519822f0aa32
Ameer J <ameer.ibrahimosman@mail.mcgill.ca>
2020-11-09 23:56:19 -0500

    cleanup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,6 @@ function Footer() {
   return (
     <div className='footer-container'>
       <section className='footer-subscription'>
-        {/* <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
-        </p> */}
         <p className='footer-subscription-text'>
           Contact Me!
         </p>
